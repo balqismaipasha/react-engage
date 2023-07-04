4fdb1c704635426360ec56fe80fcdcdf2f017cad
Balqis Maipasha <64353063+balqismaipasha@users.noreply.github.com>
2023-07-04 13:15:20 +0700

    update

--- src/services/Login.js
+++ src/services/Login.js
@@ -1,17 +1,19 @@
 import appConfig from "../config/variable";
 import * as CryptoJS from "crypto-js";
 
-// key
-const k = "/B?E(H+MbQeThWmZq4t6w9z$C&F)J@Nc";
-const kp = "aQV]4-bqEMa_pGUU$ZzAitb}HlB|.r";
+const API_KEY_KP = process.env.REACT_APP_API_KEY_Q;
+const API_KEY_K = process.env.REACT_APP_API_KEY_P;
 
 export function loginAuth(username, password) {
-  let passwordEncrypt = CryptoJS.AES.encrypt(JSON.stringify(password), kp);
+  let passwordEncrypt = CryptoJS.AES.encrypt(
+    JSON.stringify(password),
+    API_KEY_KP
+  );
   let requestBody = {
     username: username,
     password: passwordEncrypt.toString(),
   };
-  let ciphertext = CryptoJS.AES.encrypt(JSON.stringify(requestBody), k);
+  let ciphertext = CryptoJS.AES.encrypt(JSON.stringify(requestBody), API_KEY_K);
   const requestOptions = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -20,26 +22,3 @@ export function loginAuth(username, password) {
   console.log(requestOptions);
   return fetch(appConfig.API + "/login", requestOptions);
 }
-
-// export function loginAuth(username, password) {
-//   let passwordEncrypt = CryptoJS.AES.encrypt(
-//     JSON.stringify(password),
-//     process.env.REACT_APP_API_KEY_Q
-//   );
-//   let requestBody = {
-//     username: username,
-//     password: passwordEncrypt.toString(),
-//   };
-//   let ciphertext = CryptoJS.AES.encrypt(
-//     JSON.stringify(requestBody),
-//     process.env.REACT_APP_API_KEY_P
-//   );
-//   const requestOptions = {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: ciphertext.toString(),
-//   };
-
-//   console.log(requestOptions);
-//   return fetch(appConfig.API + "/login", requestOptions);
-// }
