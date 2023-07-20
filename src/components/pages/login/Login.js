fa1df0874874b837202c495278e5de03a3512281
Balqis Maipasha <64353063+balqismaipasha@users.noreply.github.com>
2023-07-20 11:30:49 +0700

    className in another file css

--- src/components/pages/login/Login.js
+++ src/components/pages/login/Login.js
@@ -1,68 +1,31 @@
 import { useState } from "react";
 import { Container, Form, Card, Button } from "react-bootstrap";
-import { loginAuth } from "../../../services/Login";
+import { loginAuth } from "../../../services/LoginServices";
+import { cookiesSecure } from "../../../config/variable";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 function Login() {
   const [Email, SetEmail] = useState();
   const [Password, SetPassword] = useState();
 
   return (
-    <Container style={{ padding: "10% 30% 10% 30%" }}>
-      <Card style={{ padding: "3% 2% 3% 2%" }}>
+    <Container className="container-login">
+      <Card className="card-header">
         <Card.Body>
-          <p
-            style={{
-              textAlign: "left",
-              fontSize: "18px",
-              marginBottom: "0",
-              color: "navy",
-            }}
-          >
-            Welcome
-          </p>
-          <p style={{ textAlign: "left", fontSize: "14px" }}>
-            Sign In to Engage System
-          </p>
+          <p className="lableLarger"> Welcome </p>
+          <p className="lable"> Sign In to Engage System </p>
           <Form>
             <Form.Group className="mb-3" controlId="userNameId">
-              <label
-                style={{
-                  textAlign: "left",
-                  width: "100%",
-                  fontSize: "14px",
-                  marginTop: "1rem",
-                }}
-              >
-                Username / E-mail
-              </label>
-              <Form.Control
-                ref={(input) => SetEmail(input?.value)}
-                onChange={(e) => SetEmail(e.target.value)}
-                type="email"
-                placeholder="difinite@gmail.com"
-              />
+              <label className="lable" > Username / E-mail </label>
+              <Form.Control ref={(input) => SetEmail(input?.value)} onChange={(e) => SetEmail(e.target.value)} type="email" placeholder="difinite@gmail.com" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="passwordId">
-              <label
-                style={{ textAlign: "left", width: "100%", fontSize: "14px" }}
-              >
-                Password
-              </label>
-              <Form.Control
-                ref={(input) => SetPassword(input?.value)}
-                onChange={(e) => SetPassword(e.target.value)}
-                type="password"
-                placeholder="********"
-              />
+              <label className="lable"> Password </label>
+              <Form.Control ref={(input) => SetPassword(input?.value)} onChange={(e) => SetPassword(e.target.value)} type="password" placeholder="********" />
             </Form.Group>
             <div className="d-grid gap-2">
-              <Button
-                variant="primary"
-                size="sm"
-                onClick={(e) => Auth(Email, Password)}
-              >
-                SIGN IN
-              </Button>
+              <Button variant="primary" size="sm" onClick={(e) => Auth(Email, Password)}> SIGN IN </Button>
             </div>
           </Form>
         </Card.Body>
@@ -72,16 +35,23 @@ function Login() {
 }
 
 function Auth(Email, Password) {
+  const navigate = useNavigate();
+
   if (Email !== "" && Password !== "") {
     loginAuth(Email, Password)
       .then((response) => response.json())
       .then((data) => {
-        if (data["error_schema"]["error_code"] === "S001") {
-          // set cookies
-          // tampilin toast berhasil
-          // pindah navigate ke home
-        } else {
-        }
+        setTimeout(() => {
+          if (data["error_schema"]["error_code"] === "S001") {
+            // set cookies
+            this.cookie.set("ACCESS_TOKEN", data["output_schema"]["access_token"], undefined, "/", undefined, cookiesSecure, "Lax");
+            this.cookie.set("REFRESH_TOKEN", data["output_schema"]["refresh_token"], undefined, "/", undefined, cookiesSecure, "Lax");
+            navigate(0);
+            // tampilin toast berhasil
+            // pindah navigate ke home
+          } else {
+          }
+        }, 2000);
       });
   }
 }
